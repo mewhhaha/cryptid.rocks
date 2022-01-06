d67abb43d0ff326047ccdfa8018d7a46d12e2c3d
mewhhaha <kopatheonlyone@hotmail.com>
2022-01-07 00:30:38 +0100

    Use normal globals

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -2,15 +2,22 @@ import { Authenticator } from "remix-auth";
 import { Auth0Profile, Auth0Strategy } from "remix-auth-auth0";
 import { createCookieSessionStorage } from "remix";
 
+declare global {
+  const SESSION_SECRET: string;
+  const AUTH0_CALLBACK_URL: string;
+  const AUTH0_CLIENT_ID: string;
+  const AUTH0_CLIENT_SECRET: string;
+  const AUTH0_DOMAIN: string;
+  const NODE_ENV: string;
+}
 
+// const { SESSION_SECRET, AUTH0_CALLBACK_URL, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_DOMAIN, NODE_ENV } = process.env
 
-const { SESSION_SECRET, AUTH0_CALLBACK_URL, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_DOMAIN, NODE_ENV } = process.env
-
-if (SESSION_SECRET === undefined) throw new Error("SESSION_SECRET is not defined")
-if (AUTH0_CALLBACK_URL === undefined) throw new Error("AUTH0_CALLBACK_URL is not defined")
-if (AUTH0_CLIENT_ID === undefined) throw new Error("AUTH0_CLIENT_ID is not defined")
-if (AUTH0_CLIENT_SECRET === undefined) throw new Error("AUTH0_CLIENT_SECRET is not defined")
-if (AUTH0_DOMAIN === undefined) throw new Error("AUTH0_DOMAIN is not defined")
+// if (SESSION_SECRET === undefined) throw new Error("SESSION_SECRET is not defined")
+// if (AUTH0_CALLBACK_URL === undefined) throw new Error("AUTH0_CALLBACK_URL is not defined")
+// if (AUTH0_CLIENT_ID === undefined) throw new Error("AUTH0_CLIENT_ID is not defined")
+// if (AUTH0_CLIENT_SECRET === undefined) throw new Error("AUTH0_CLIENT_SECRET is not defined")
+// if (AUTH0_DOMAIN === undefined) throw new Error("AUTH0_DOMAIN is not defined")
 
 
 // export the whole sessionStorage object

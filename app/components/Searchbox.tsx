9199b14d56e2b1d38d4537122fdcedfee1576f89
mewhhaha <kopatheonlyone@hotmail.com>
2022-01-07 20:40:33 +0100

    LALALAL

--- app/components/Searchbox.tsx
+++ app/components/Searchbox.tsx
@@ -1,13 +1,11 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Transition } from "@headlessui/react";
 import {
   Combobox,
-  ComboboxInput,
-  useComboboxContext,
+  useComboboxState,
   ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
+  ComboboxItem,
+} from "ariakit/combobox";
 import coinList from "~/data/coins.json";
 import cn from "classnames";
 import { Setter } from "~/types";
@@ -104,138 +102,72 @@ const useCoinList = (query: string) => {
   return list;
 };
 
-const isVisible = (el: HTMLElement, container: HTMLElement) => {
-  const elTop = el.offsetTop;
-  const elBottom = elTop + el.clientHeight;
-
-  const containerTop = container.scrollTop;
-  const containerBottom = containerTop + container.clientHeight;
-
-  return (
-    (elTop >= containerTop && elBottom <= containerBottom) ||
-    (elTop < containerTop && containerTop < elBottom) ||
-    (elTop < containerBottom && containerBottom < elBottom)
-  );
-};
-
-type SearchboxContentProps = {
-  coinList: CoinInfo[];
-  search: string;
-  setSearch: Setter<string>;
-};
-
-const SearchboxContent: React.VFC<SearchboxContentProps> = ({
-  coinList,
-  search,
-  setSearch,
-}) => {
-  const { navigationValue } = useComboboxContext();
-  const [focus, setFocus] = useState(false);
-
-  const activeRef = useRef<HTMLLIElement>(null);
-  const listRef = useRef<HTMLUListElement>(null);
-
-  useEffect(() => {
-    if (
-      !activeRef.current ||
-      !listRef.current ||
-      isVisible(activeRef.current, listRef.current)
-    )
-      return;
-
-    const el = activeRef.current;
-    const container = listRef.current;
-    const isAbove = el.offsetTop + el.clientHeight <= container.scrollTop;
-    activeRef.current.scrollIntoView(isAbove);
-  }, [navigationValue]);
-
-  return (
-    <div className="relative mt-1">
-      <ComboboxInput
-        className="relative w-full px-8 py-2 text-xl text-left bg-white rounded-full shadow-md outline-none cursor-default sm:text-2xl focus:ring-2 focus:ring-opacity-75 focus:ring-blue-400 focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500"
-        value={search}
-        placeholder="Search for crypto to add"
-        onFocus={() => setFocus(true)}
-        onBlur={() => setFocus(false)}
-        onChange={(e) => setSearch(() => e.target.value)}
-      />
-
-      <Transition
-        show={focus}
-        as="div"
-        leave="transition ease-in duration-100"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      >
-        <ComboboxPopover>
-          <ComboboxList
-            ref={listRef}
-            className="absolute w-full py-1 mt-4 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-          >
-            {coinList.length === 0 ? (
-              <li className="px-4 py-2">No results</li>
-            ) : (
-              coinList.map((coin, index) => {
-                const active = coin.name === navigationValue;
-                return (
-                  <ComboboxOption
-                    key={index}
-                    ref={active ? activeRef : undefined}
-                    className={cn(
-                      active
-                        ? "text-yellow-900 bg-yellow-100"
-                        : "text-gray-900",
-                      "cursor-default select-none relative py-2 px-4 hover:bg-gray-200"
-                    )}
-                    value={coin.name}
-                  >
-                    <div className="flex w-full">
-                      <span className="flex-grow min-w-0 truncate">
-                        {coin.name}
-                      </span>
-                      <span
-                        className={cn(
-                          active ? "text-yellow-600" : "text-gray-400",
-                          "self-end text-right items-center pr-3"
-                        )}
-                      >
-                        {coin.symbol.toLocaleUpperCase()}
-                      </span>
-                    </div>
-                  </ComboboxOption>
-                );
-              })
-            )}
-          </ComboboxList>
-        </ComboboxPopover>
-      </Transition>
-    </div>
-  );
-};
-
 type SearchboxProps = {
   setPortfolio: Setter<PortfolioCoin[]>;
 };
 
 export const Searchbox: React.VFC<SearchboxProps> = ({ setPortfolio }) => {
-  const [search, setSearch] = useState("");
-  const coinList = useCoinList(search);
+  const combobox = useComboboxState();
+  const coinList = useCoinList(combobox.value);
 
-  const handleSelect = (coinName: string) => {
-    const coin = coinLookup[coinName];
-    setPortfolio((portfolio) => {
-      if (portfolio.find((x) => x.id === coin.id)) return portfolio;
-      return [...portfolio, { ...coin, quantity: 0 }];
-    });
-    setSearch("");
-  };
+  const handleOnChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const coinName = e.target.value;
+      const coin = coinLookup[coinName];
+      setPortfolio((portfolio) => {
+        if (portfolio.find((x) => x.id === coin.id)) return portfolio;
+        return [...portfolio, { ...coin, quantity: 0 }];
+      });
+      combobox.setValue("");
+    },
+    [combobox, setPortfolio]
+  );
+
   return (
-    <Combobox as="div" className="w-full max-w-xl" onSelect={handleSelect}>
-      <SearchboxContent
-        coinList={coinList}
-        search={search}
-        setSearch={setSearch}
+    <div className="relative mt-1">
+      <Combobox
+        className="relative w-full px-8 py-2 text-xl text-left bg-white rounded-full shadow-md outline-none cursor-default sm:text-2xl focus:ring-2 focus:ring-opacity-75 focus:ring-blue-400 focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500"
+        state={combobox}
+        onChange={handleOnChange}
+        placeholder="Search for crypto to add"
       />
-    </Combobox>
+
+      <ComboboxPopover
+        state={combobox}
+        className="absolute w-full py-1 mt-4 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+      >
+        {coinList.length === 0 ? (
+          <li className="px-4 py-2">No results</li>
+        ) : (
+          coinList.map((coin, index) => {
+            const active = coin.name === combobox.value;
+            return (
+              <ComboboxItem
+                key={index}
+                className={cn(
+                  active ? "text-yellow-900 bg-yellow-100" : "text-gray-900",
+                  "cursor-default select-none relative py-2 px-4 hover:bg-gray-200"
+                )}
+                value={coin.name}
+              >
+                <div className="flex w-full">
+                  <span className="flex-grow min-w-0 truncate">
+                    {coin.name}
+                  </span>
+                  <span
+                    className={cn(
+                      active ? "text-yellow-600" : "text-gray-400",
+                      "self-end text-right items-center pr-3"
+                    )}
+                  >
+                    {coin.symbol.toLocaleUpperCase()}
+                  </span>
+                </div>
+              </ComboboxItem>
+            );
+          })
+        )}
+      </ComboboxPopover>
+    </div>
   );
 };

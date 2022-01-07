76cca83fa0c3b7c45f8a1914152d14a30376f758
mewhhaha <kopatheonlyone@hotmail.com>
2022-01-07 15:41:02 +0100

    Redirect

--- app/routes/auth/login.tsx
+++ app/routes/auth/login.tsx
@@ -1,13 +1,9 @@
 import { auth } from "~/services/auth.server";
-import { ActionFunction, LoaderFunction } from "~/types";
+import { ActionFunction } from "~/types";
 
-export const loader: LoaderFunction = async ({ request, context }) => {
+export const action: ActionFunction = async ({ request, context }) => {
   await auth(context).authenticate("auth0", request, {
     successRedirect: "/",
     failureRedirect: "/login",
   });
-};
-
-export const action: ActionFunction = async ({ request, context }) => {
-  await auth(context).authenticate("auth0", request);
 };

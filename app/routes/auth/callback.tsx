5af3208291f448e4be99724462192943f014ac04
mewhhaha <kopatheonlyone@hotmail.com>
2022-01-07 02:12:03 +0100

    Just await

--- app/routes/auth/callback.tsx
+++ app/routes/auth/callback.tsx
@@ -1,8 +1,8 @@
 import { LoaderFunction } from "remix";
 import { auth } from "~/services/auth.server";
 
-export const loader: LoaderFunction = ({ request, context }) => {
-  return auth(context).authenticate("auth0", request, {
+export const loader: LoaderFunction = async ({ request, context }) => {
+  await auth(context).authenticate("auth0", request, {
     successRedirect: "/",
     failureRedirect: "/login",
   });
--- app/routes/auth/logout.tsx
+++ app/routes/auth/logout.tsx
@@ -1,6 +1,6 @@
 import { LoaderFunction } from "remix";
 import { auth } from "~/services/auth.server";
 
-export const loader: LoaderFunction = ({ request, context }) => {
-  return auth(context).logout(request, { redirectTo: "/login" });
+export const loader: LoaderFunction = async ({ request, context }) => {
+  await auth(context).logout(request, { redirectTo: "/login" });
 };

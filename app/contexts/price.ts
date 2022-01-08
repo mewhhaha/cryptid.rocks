eafa3a21711c7ad19463e45fbdc86feecdba9883
mewhhaha <kopatheonlyone@hotmail.com>
2022-01-08 02:01:59 +0100

    Allow optional price

--- app/contexts/price.ts
+++ app/contexts/price.ts
@@ -6,10 +6,5 @@ const Context = createContext<SimplePrice<"sek"> | undefined>(undefined)
 export const PriceProvider = Context.Provider
 
 export const usePrice = () => {
-  const context = useContext(Context)
-  if (context === undefined) {
-    throw new Error("Expected PriceProvider to wrap usePrice hook")
-  }
-
-  return context;
+  return useContext(Context);
 }

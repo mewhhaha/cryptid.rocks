77c826280169bf301cec0773435e3ed79b54dc8b
mewhhaha <kopatheonlyone@hotmail.com>
2022-01-08 02:25:38 +0100

    Change back to span

--- app/components/Portfolio/PortfolioValueChange.tsx
+++ app/components/Portfolio/PortfolioValueChange.tsx
@@ -10,17 +10,17 @@ export const PortfolioValueChange: React.VFC<PortfolioValueChangeProps> = ({
 }) => {
   if (change < 0) {
     return (
-      <div className="flex justify-center flex-none w-32 px-3 text-yellow-800 bg-red-100 rounded-full">
+      <span className="flex justify-center flex-none w-32 px-3 text-yellow-800 bg-red-100 rounded-full">
         <ArrowSmDownIcon className="self-center w-5 h-5" />
         {`${Math.abs(change).toFixed(3)}%`}
-      </div>
+      </span>
     );
   } else {
     return (
-      <div className="flex justify-center flex-none w-32 px-3 text-green-600 bg-green-100 rounded-full">
+      <span className="flex justify-center flex-none w-32 px-3 text-green-600 bg-green-100 rounded-full">
         <ArrowSmUpIcon className="self-center w-5 h-5" />
         {`${change.toFixed(3)}%`}
-      </div>
+      </span>
     );
   }
 };

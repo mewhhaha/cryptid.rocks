4db2fe16f440514740f983a91a5152757a0bcd64
mewhhaha <kopatheonlyone@hotmail.com>
2022-01-24 14:13:32 +0100

    Fix logout session properly

--- app/routes/auth/logout.tsx
+++ app/routes/auth/logout.tsx
@@ -2,5 +2,7 @@ import { logout } from "~/services/auth.server";
 import { ActionFunction } from "~/types";
 
 export const action: ActionFunction = async ({ context }) => {
-  await logout(context, { redirectTo: "/login" });
+  await logout(context, {
+    redirectTo: `https://${context.env.AUTH0_DOMAIN}/v2/logout?client_id=${context.env.AUTH0_CLIENT_ID}`,
+  });
 };

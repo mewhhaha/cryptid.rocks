f7c40b61b695fad17b7c9279ca08dc962f2fd191
mewhhaha <kopatheonlyone@hotmail.com>
2022-01-24 14:35:15 +0100

    Update url

--- app/routes/auth/logout.tsx
+++ app/routes/auth/logout.tsx
@@ -2,7 +2,7 @@ import { logout } from "~/services/auth.server";
 import { ActionFunction } from "~/types";
 
 export const action: ActionFunction = async ({ context }) => {
-  const url = new URL("https://${context.env.AUTH0_DOMAIN}/v2/logout");
+  const url = new URL(`https://${context.env.AUTH0_DOMAIN}/v2/logout`);
   url.searchParams.set("client_id", context.env.AUTH0_CLIENT_ID);
   url.searchParams.set("callback_url", new URL(context.request.url).origin);
 

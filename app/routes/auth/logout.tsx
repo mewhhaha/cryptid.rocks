2d123360232527f3b384b49c45477766c197ec1a
mewhhaha <kopatheonlyone@hotmail.com>
2022-01-24 14:34:46 +0100

    Update

--- app/routes/auth/logout.tsx
+++ app/routes/auth/logout.tsx
@@ -2,9 +2,11 @@ import { logout } from "~/services/auth.server";
 import { ActionFunction } from "~/types";
 
 export const action: ActionFunction = async ({ context }) => {
-  await logout(context, {
-    redirectTo: `https://${context.env.AUTH0_DOMAIN}/v2/logout?client_id=${
-      context.env.AUTH0_CLIENT_ID
-    }&callback_url=${new URL(context.request.url).origin}`,
+  const url = new URL("https://${context.env.AUTH0_DOMAIN}/v2/logout");
+  url.searchParams.set("client_id", context.env.AUTH0_CLIENT_ID);
+  url.searchParams.set("callback_url", new URL(context.request.url).origin);
+
+  return await logout(context, {
+    redirectTo: url.toString(),
   });
 };

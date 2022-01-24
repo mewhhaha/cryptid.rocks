6bbc48c27f68f3ba4ee7bee0140ab4833db34133
mewhhaha <kopatheonlyone@hotmail.com>
2022-01-24 14:28:28 +0100

    Remove the /login

--- app/routes/auth/logout.tsx
+++ app/routes/auth/logout.tsx
@@ -5,6 +5,6 @@ export const action: ActionFunction = async ({ context }) => {
   await logout(context, {
     redirectTo: `https://${context.env.AUTH0_DOMAIN}/v2/logout?client_id=${
       context.env.AUTH0_CLIENT_ID
-    }&callback_url=${new URL(context.request.url).origin}/login`,
+    }&callback_url=${new URL(context.request.url).origin}`,
   });
 };

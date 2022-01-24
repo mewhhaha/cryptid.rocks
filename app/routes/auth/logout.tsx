bf95c5b7ca9f512032a7e16a24ccf451689e205e
mewhhaha <kopatheonlyone@hotmail.com>
2022-01-24 14:44:29 +0100

    Push to login

--- app/routes/auth/logout.tsx
+++ app/routes/auth/logout.tsx
@@ -4,7 +4,10 @@ import { ActionFunction } from "~/types";
 export const action: ActionFunction = async ({ context }) => {
   const url = new URL(`https://${context.env.AUTH0_DOMAIN}/v2/logout`);
   url.searchParams.set("client_id", context.env.AUTH0_CLIENT_ID);
-  url.searchParams.set("returnTo", new URL(context.request.url).origin);
+  url.searchParams.set(
+    "returnTo",
+    `${new URL(context.request.url).origin}/login`
+  );
 
   return await logout(context, {
     redirectTo: url.toString(),

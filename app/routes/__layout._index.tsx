db5bda930163faf82de19ba27c870c0f4da4b90b
mewhhaha <kopatheonlyone@hotmail.com>
2024-09-06 23:31:18 +0200

    authenticate immediately

--- app/routes/__layout._index.tsx
+++ app/routes/__layout._index.tsx
@@ -1,5 +1,10 @@
-import { redirect } from "@remix-run/cloudflare";
+import { LoaderFunctionArgs, redirect } from "@remix-run/cloudflare";
+import { authenticate } from "~/helpers/auth.server";
 
-export const loader = () => {
+export const loader = ({
+  request,
+  context: { cloudflare: cf },
+}: LoaderFunctionArgs) => {
+  const _ = authenticate(cf, request);
   return redirect("/coins");
 };
